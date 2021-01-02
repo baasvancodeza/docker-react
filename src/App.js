d5b1b44f31940d88e39819000d5024fac95b00dd
Stefan Cronje <baasvancode@gmail.com>
2021-01-02 08:52:10 +0000

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there
+          Test full git flow
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This was changed on the development git branch
+          Learn React
         </a>
       </header>
     </div>
